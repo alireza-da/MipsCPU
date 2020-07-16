434de38f91b4e3070ea91d0af0e4c64d26085a5e
Brainless18 <alirezadavoodi1378@gmail.com>
2020-07-16 17:05:06 +0430

    Register updated

---
+++ src/simulator/Sample.java
@@ -0,0 +1,19 @@
+
+
+package simulator;
+
+import simulator.control.Simulator;
+import simulator.wrapper.wrappers.*;
+
+public class Sample {
+    public static void main(String[] args) {
+        //sample circuit
+        Adder adder = new Adder("ADDER", "10X6",
+                Simulator.falseLogic, Simulator.trueLogic, Simulator.trueLogic, Simulator.falseLogic, Simulator.trueLogic,
+                Simulator.falseLogic, Simulator.trueLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.trueLogic);
+
+        Simulator.debugger.addTrackItem(adder);
+        Simulator.debugger.setDelay(200);
+        Simulator.circuit.startCircuit();
+    }
+}
