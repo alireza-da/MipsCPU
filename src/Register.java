576b15429990ba41a49420855e7cea148d076be8
Brainless18 <alirezadavoodi1378@gmail.com>
2020-07-18 19:55:47 +0430

    write reg

--- src/Register.java
+++ src/Register.java
@@ -8,35 +8,53 @@ import java.util.ArrayList;
 
 public class Register{
     //private Clock clock;
-    private ArrayList<FlipFlop> flipFlops = new ArrayList<>();
+    private ArrayList<DFlipFlop> flipFlops;
+    private Link write = Simulator.falseLogic;
+    private Link read = Simulator.falseLogic;
 
+    public Link getRead() {
+        return read;
+    }
 
-    public void setFlipFlops(ArrayList<FlipFlop> flipFlops) {
+    public void setRead(Link read) {
+        this.read = read;
+    }
+
+    public Link getWrite() {
+        return write;
+    }
+
+    public void setWrite(Link write) {
+        this.write = write;
+    }
+
+    public void setFlipFlops(ArrayList<DFlipFlop> flipFlops) {
         this.flipFlops = flipFlops;
     }
 
-    public ArrayList<FlipFlop> getFlipFlops() {
+    public ArrayList<DFlipFlop> getFlipFlops() {
         return flipFlops;
     }
 
-    /*public void setClock(Clock clock) {
-        this.clock = clock;
-    }
-
-    public Clock getClock() {
-        return clock;
-
-    }*/
 
     public void setFlipFlopsByIndex(int startIndex, int endIndex, int[] data, Clock clock){
-        for (int i=startIndex ; i<endIndex ; i++ ){
-            if (data[i] == 0){
-                getFlipFlops().set(i, new DFlipFlop("d"+i, clock.getOutput(0),Simulator.falseLogic));
-            }
-            else if (data[i] == 1){
-                getFlipFlops().set(i, new DFlipFlop("d"+i, clock.getOutput(0),Simulator.trueLogic));
+            for (int i = startIndex; i < endIndex; i++) {
+                if (data[i] == 0) {
+                    getFlipFlops().set(i, new DFlipFlop("d" + i, clock.getOutput(0), Simulator.falseLogic));
+                } else if (data[i] == 1) {
+                    getFlipFlops().set(i, new DFlipFlop("d" + i, clock.getOutput(0), Simulator.trueLogic));
+                }
             }
 
+    }
+
+
+    public void write(ArrayList<Link> links,Clock clock){
+        if(write == Simulator.trueLogic){
+            int i = 0;
+            for (Link link : links) {
+                getFlipFlops().set(i, new DFlipFlop("d" + i, clock.getOutput(0), link));
+            }
         }
     }
 
@@ -54,7 +72,7 @@ public class Register{
         Clock c = new Clock("c", 1000);
         for (int j = 0; j < 32; j++) {
             assert false;
-            DFlipFlop d = new DFlipFlop("d"+ j, c.getOutput(0), Simulator.falseLogic);
+            DFlipFlop d = new DFlipFlop("d"+ j, c.getOutput(0));
 
             register.getFlipFlops().add(d);
         }
--- src/RegisterFile.java
+++ src/RegisterFile.java
@@ -9,14 +9,14 @@ import java.util.HashMap;
 
 public class RegisterFile extends Wrapper {
     private HashMap<Integer,Register> rf = new HashMap<>();
-    //regWrite signal , register read 1 , 2 ; register write , write data
+    //clock cycle , regWrite signal , register read 1 , 2 ; register write , write data ( ordination of inputs )
     public RegisterFile(String label, String stream, Link... links){
         super(label,stream,links);
         for (int i = 0 ; i < 32 ; i++){
             Register r = new Register();
 
             for (int j = 0; j < 32; j++) {
-                assert false;
+
                 DFlipFlop d = new DFlipFlop("d"+ j, getInput(0), Simulator.falseLogic);
 
                 r.getFlipFlops().add(d);
@@ -30,7 +30,40 @@ public class RegisterFile extends Wrapper {
 
     @Override
     public void initialize() {
+        // write
 
+        And a1 = new And("and1",getInput(1));
+        And a2 = new And("and2",getInput(1));
+        And a3 = new And("and3",getInput(1));
+        And a4 = new And("and4",getInput(1));
+        And a5 = new And("and5",getInput(1));
+        And a6 = new And("and6",getInput(1));
+        And a7 = new And("and7",getInput(1));
+        And a8 = new And("and8",getInput(1));
+        And a9 = new And("and9",getInput(1));
+        And a10 = new And("and10",getInput(1));
+        And a11 = new And("and11",getInput(1));
+        And a12 = new And("and12",getInput(1));
+        And a13 = new And("and13",getInput(1));
+        And a14 = new And("and14",getInput(1));
+        And a15 = new And("and15",getInput(1));
+        And a16 = new And("and16",getInput(1));
+        And a17 = new And("and17",getInput(1));
+        And a18 = new And("and18",getInput(1));
+        And a19 = new And("and19",getInput(1));
+        And a20 = new And("and20",getInput(1));
+        And a21 = new And("and21",getInput(1));
+        And a22 = new And("and22",getInput(1));
+        And a23 = new And("and23",getInput(1));
+        And a24 = new And("and24",getInput(1));
+        And a25 = new And("and25",getInput(1));
+        And a26 = new And("and26",getInput(1));
+        And a27 = new And("and27",getInput(1));
+        And a28 = new And("and28",getInput(1));
+        And a29 = new And("and29",getInput(1));
+        And a30 = new And("and30",getInput(1));
+        And a31 = new And("and31",getInput(1));
+        And a32 = new And("and32",getInput(1));
 
     }
 
